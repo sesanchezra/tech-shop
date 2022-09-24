81cebde3aaa38be12f79bbe68ccd2cba20ef4000
Sebastian Sanchez <sesanchezra@unal.edu.co>
2022-09-24 00:47:45 -0500

    Homepage Search functionality, ready

--- src/components/Home/HomePage/HomePage.jsx
+++ src/components/Home/HomePage/HomePage.jsx
@@ -7,6 +7,11 @@ import { FiSearch } from "react-icons/fi";
 import axios from 'axios';
 import Category from '../Category/Category';
 import ProductCard from '../ProductCard/ProductCard';
+import { useForm } from 'react-hook-form';
+
+const defaultValue = {
+    search: ''
+}
 
 
 const HomePage = () => {
@@ -23,18 +28,50 @@ const HomePage = () => {
     }, [])
 
     const [products, setProducts] = useState()
+    const [allClick, setallClick] = useState(false)
 
     useEffect(() => {
-        const URL ='https://ecommerce-api-react.herokuapp.com/api/v1/products'
+        const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/products'
         axios.get(URL)
             .then(res => setProducts(res.data.data.products))
             .catch(error => console.log(error))
-    }, [])
+    }, [allClick])
 
-    console.log(products)
+
 
     const [categoryActive, setCategoryActive] = useState('All')
 
+
+    //Search Functionality
+
+    const { handleSubmit, register, reset } = useForm()
+
+    const [searchActive, setSearchActive] = useState(false)
+
+    const [searchResult, setSearchResult] = useState()
+
+    const search = (data) => {
+        setSearchActive(true)
+        const arraySearch = data.search?.toLowerCase().split(' ')
+
+        let searchResult = products?.filter(product => {
+            let arrayTitle = product.title.toLowerCase().split(' ')
+            for (let i = 0; i < arraySearch.length; i++) {
+                for (let j = 0; j < arrayTitle.length; j++) {
+                    if (arraySearch[i] === arrayTitle[j]) {
+                        return true
+                    }
+
+                }
+            }
+
+        })
+
+        let filter = new Set(searchResult);
+        let result = [...filter]
+        setSearchResult(result)
+        reset(defaultValue)
+    }
 
     return (
         <div className='HomePage'>
@@ -59,9 +96,9 @@ const HomePage = () => {
                 </div>
             </div>
 
-            <form className='search'>
+            <form className='search' onSubmit={handleSubmit(search)}>
 
-                <input type="text" placeholder='Search' />
+                <input type="text" placeholder='Search' {...register('search')} />
                 <IconContext.Provider value={{ size: '1.8em', color: 'rgb(35, 38, 45,0.6)' }}>
                     <button className='search__button'>
                         <FiSearch />
@@ -72,39 +109,67 @@ const HomePage = () => {
             </form>
 
             <div className='categories'>
-                <Category
-                    key={'all'}
-                    category={
-                        {
-                            "id": 0,
-                            "name": 'All',
-                            "status": "active"
-                        }
-                    }
-                    setCategoryActive={setCategoryActive}
-                    categoryActive={categoryActive}
-                />
                 {
-                    categories?.map(category => (
+                    searchActive ?
                         <Category
-                            key={category.id}
-                            category={category}
+                            key={'all'}
+                            category={
+                                {
+                                    "id": 0,
+                                    "name": 'All',
+                                    "status": "active"
+                                }
+                            }
                             setCategoryActive={setCategoryActive}
                             categoryActive={categoryActive}
                         />
-                    ))
+                    :
+                        <>
+                            <Category
+                                key={'all'}
+                                category={
+                                    {
+                                        "id": 0,
+                                        "name": 'All',
+                                        "status": "active"
+                                    }
+                                }
+                                setCategoryActive={setCategoryActive}
+                                categoryActive={categoryActive}
+                            />
+                            {
+                                categories?.map(category => (
+                                    <Category
+                                        key={category.id}
+                                        category={category}
+                                        setCategoryActive={setCategoryActive}
+                                        categoryActive={categoryActive}
+                                    />
+                                ))
+                            }
+                        </>
+
                 }
 
             </div>
 
             <div className='products'>
+
                 {
-                    products?.map(product => (
-                        <ProductCard 
-                            key={product.title}
-                            product={product}                        
-                        />
-                    ))
+                    searchActive ?
+                        searchResult?.map(product => (
+                            <ProductCard
+                                key={product.title}
+                                product={product}
+                            />
+                        ))
+                        :
+                        products?.map(product => (
+                            <ProductCard
+                                key={product.title}
+                                product={product}
+                            />
+                        ))
                 }
             </div>
         </div>
